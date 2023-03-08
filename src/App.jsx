eace66bc605e58df8c464a4207df06680b7bef28
Yeikol Villalobos <yeikolvillalobos27@gmail.com>
2023-03-07 22:32:02 -0600

    files

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { useRef, useState } from "react"
+import { youtube_parser } from "./utils";
+
+function App() {
+  const inputUrlRef = useRef();
+  const [urlResult, setUrlResult] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const youtubeID = youtube_parser(inputUrlRef.current.value);
+
+    const options = {
+      method: 'get',
+      url: 'https://youtube-mp36.p.rapidapi.com/dl',
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+      },
+      params: {
+        id: youtubeID
+      }
+    }
+    axios(options)
+      .then(res => 
+        setUrlResult(res.data),
+      )
+      .catch(err => console.log(err))
+
+    inputUrlRef.current.value = '';
+
+  }
+
+  return (
+    <div className="app">
+    <span className="logo">Youtube Downloader</span>
+    <section className="content">
+      <h1 className="content_title">YouTube to MP3 Converter</h1>
+      <p className="content_description">
+        Transform YouTube videos into MP3s in just a few clicks!
+      </p>
+
+      <form onSubmit={handleSubmit} className="form">
+        <input ref={inputUrlRef} placeholder="Paste a Youtube video URL link..." className="form_input" type="text" />
+        <button type="submit" className="form_button">Search</button>
+      </form>
+      {urlResult ?  <p  className="sound_title" >{urlResult.title} &#9989;</p>: ''}
+      {urlResult ? <a target='_blank' rel="noreferrer" href={urlResult.link} className="download_btn">Download MP3</a> : ''}
+      
+    </section>
+  </div>
+)
+}
+export default App
